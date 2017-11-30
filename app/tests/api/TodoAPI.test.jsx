2ca272b9f4a4d0bf6dae685b9048b1bd832edf4e
Kubyustus <neriad@gmail.com>
2017-11-30 11:33:43 +0200

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -53,4 +53,46 @@ describe('TodoAPI', () => {
     });
 
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'some text here',
+      completed: true
+    },{
+      id: 2,
+      text: 'second some text here',
+      completed: false
+    },{
+      id: 3,
+      text: 'third text here',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed totos if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort todo by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by search text', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+  });
 });
