f5518566d2af6169fa0ed4abe1efde947df8e378
Kubyustus <neriad@gmail.com>
2017-11-30 16:10:00 +0200

    add completed and created timestampls

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,13 +19,16 @@ describe('TodoApp', () => {
     todoApp.HandleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: 'Test features',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
 
@@ -38,6 +41,27 @@ describe('TodoApp', () => {
 
     //Verify that value of completed has changed to true
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  //test that when toggle from true to false , completedAt get removed
+  it('should remove completedAt when toggling completed value from true to false', () => {
+    var todoData = {
+      id: 12,
+      text: 'Test features2',
+      completed: true,
+      createdAt: 500,
+      completedAt: 510
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: [todoData]});
+
+    //call handleToggle with 11
+    todoApp.handleToggle(12);
+
+    // expect completedAt to be undefined
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 
 });
